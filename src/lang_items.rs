2a9dec870fd47ac59129096c7a4c0fb6a6a8712c
huangg2018 <867454076@qq.com>
2024-04-23 23:12:58 +0000

    update 20240424.07:12

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,10 +1,19 @@
 use core::panic::PanicInfo;
-use crate::console::*;
+use crate::println;
 use crate::sbi::shutdown;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
-        println!()
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+
+    }else {
+        println!("Panicked: {}", info.message().unwrap());
     }
+    shutdown(true)
 }
